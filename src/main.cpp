3ca9da04543072ecb27062999f1115b13c12682e
Fatih Eren Ekinci <fatiherenekinci0@gmail.com>
2026-01-08 22:57:55 +0300

    Add file I/O for persistent student storage
    Added functions to load and save student data from/to a file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 struct Student {
@@ -11,6 +12,30 @@ struct Student {
 };
 
 vector<Student> students;
+
+void loadFromFile() {
+    ifstream file("data/students.txt");
+    if (!file.is_open()) return;
+
+    Student s;
+    while (file >> s.id) {
+        file.ignore();
+        getline(file, s.name);
+        file >> s.grade1 >> s.grade2 >> s.grade3;
+        students.push_back(s);
+    }
+    file.close();
+}
+
+void saveToFile() {
+    ofstream file("data/students.txt");
+    for (const auto& s : students) {
+        file << s.id << endl;
+        file << s.name << endl;
+        file << s.grade1 << " " << s.grade2 << " " << s.grade3 << endl;
+    }
+    file.close();
+}
 
 void addStudent() {
     Student s;
@@ -29,7 +54,9 @@ void addStudent() {
     cin >> s.grade3;
 
     students.push_back(s);
-    cout << "Student added successfully!\n";
+    saveToFile();
+
+    cout << "Student added and saved successfully!\n";
 }
 
 void listStudents() {
@@ -43,8 +70,9 @@ void listStudents() {
 }
 
 int main() {
-    int choice;
+    loadFromFile();
 
+    int choice;
     do {
         cout << "\n===== Student Management System =====\n";
         cout << "1. Add Student\n";
